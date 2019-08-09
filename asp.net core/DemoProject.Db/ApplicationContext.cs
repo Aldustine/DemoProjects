08bd49d62f92656ab06d999de12dfba4d717969d
Maksym Melnyk <pr0100makcimm@gmail.com>
2019-08-10 00:27:34 +0300

    Initial commit

---
+++ asp.net core/DemoProject.Db/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProject.Data.Db
+{
+    public class ApplicationContext : DbContext 
+    {
+
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base (options)
+        {
+            Database.Migrate();
+        }
+    }
+}
---
+++ asp.net core/DemoProject/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DemoProject.Data.Db;
+
+namespace DemoProject.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MainConnection")));
+
+            //services.AddTransient<>();
+
+            services.AddMvc();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
